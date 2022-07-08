eda6b42269e7a8bd4fdf4e24f372c2662ccd5f39
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-08 15:58:30 -0600

    Initial Upload

---
+++ lab03.c
@@ -0,0 +1,12 @@
+/* lab03.c
+ * ===========================================================
+ * Name: 
+ * Section:
+ * Project: Lab 3 - Using #define and #include
+ * Purpose: Learn how #define and #include really work
+ */
+
+// What do you need to #include to use the printf/scanf functions?
+
+
+// Put Your Main Function Here
