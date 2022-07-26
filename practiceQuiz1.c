ef3f5153de3aff32105dfc6341d807fcd64835ba
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-26 15:43:16 -0600

    Uploading Practice Quiz Template

---
+++ practiceQuiz1.c
@@ -0,0 +1,9 @@
+/* practiceQuiz1.c
+ * ===========================================================
+ * Name: 
+ * Section:
+ * Project: Lab 3 - Practice Quiz (0 Points)
+ * Purpose: Practice using printf
+ */
+
+// YOUR CODE GOES HERE
---
+++ practiceQuiz2.c
@@ -0,0 +1,9 @@
+/* practiceQuiz2.c
+ * ===========================================================
+ * Name: 
+ * Section:
+ * Project: Lab 3 - Practice Quiz (0 Points)
+ * Purpose: Practice how to use printf and scanf
+ */
+
+// YOUR CODE GOES HERE
